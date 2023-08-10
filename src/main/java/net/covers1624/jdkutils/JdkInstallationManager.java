0ee52bd39e892245bfc57d7735defb04feae2475
covers1624 <laughlan.cov@internode.on.net>
2023-08-10 17:10:36 +0930

    Improve M1 support. Allow installs of aarch64 and x64 vms side-by-side.

--- src/main/java/net/covers1624/jdkutils/JdkInstallationManager.java
+++ src/main/java/net/covers1624/jdkutils/JdkInstallationManager.java
@@ -12,6 +12,8 @@ import net.covers1624.quack.collection.StreamableIterable;
 import net.covers1624.quack.gson.HashCodeAdapter;
 import net.covers1624.quack.gson.JsonUtils;
 import net.covers1624.quack.net.download.DownloadListener;
+import net.covers1624.quack.platform.Architecture;
+import net.covers1624.quack.platform.OperatingSystem;
 import net.covers1624.quack.util.HashUtils;
 import org.apache.maven.artifact.versioning.ComparableVersion;
 import org.jetbrains.annotations.Nullable;
@@ -46,21 +48,18 @@ public class JdkInstallationManager {
 
     private final Path baseDir;
     private final JdkProvisioner provisioner;
-    private final boolean ignoreMacosAArch64;
     private final Path manifestPath;
     private final List<Installation> installations;
 
     /**
      * Create a new Jdk Installation Manager.
      *
-     * @param baseDir            The base directory to install JDK's to.
-     * @param provisioner        The provisioner to provision new JDK's.
-     * @param ignoreMacosAArch64 If AArch64 macOS aarch64 should be treated as amd64.
+     * @param baseDir     The base directory to install JDK's to.
+     * @param provisioner The provisioner to provision new JDK's.
      */
-    public JdkInstallationManager(Path baseDir, JdkProvisioner provisioner, boolean ignoreMacosAArch64) {
+    public JdkInstallationManager(Path baseDir, JdkProvisioner provisioner) {
         this.baseDir = baseDir;
         this.provisioner = provisioner;
-        this.ignoreMacosAArch64 = ignoreMacosAArch64;
         manifestPath = baseDir.resolve("installations.json");
         List<Installation> installs = new ArrayList<>();
         if (Files.exists(manifestPath)) {
@@ -84,9 +83,12 @@ public class JdkInstallationManager {
      * @return The Found JDK home directory. Otherwise <code>null</code>.
      */
     @Nullable
-    public Path findJdk(JavaVersion version, @Nullable String semver, boolean jre) {
+    public Path findJdk(JavaVersion version, @Nullable String semver, boolean jre, boolean ignoreMacosAArch64) {
+        OperatingSystem os = OperatingSystem.current();
         LinkedList<Installation> candidates = StreamableIterable.of(installations)
                 .filter(e -> version == JavaVersion.parse(e.version))
+                // On mac, if we are ignoring aarch64, filter it away, otherwise only include it.
+                .filter(e -> os != OperatingSystem.MACOS || ignoreMacosAArch64 != (e.architecture == Architecture.AARCH64))
                 .filter(e -> semver == null || semver.equals(e.version)) // If we have a semver filter, do the filter.
                 .filter(e -> jre || e.isJdk) // If we require a JDK, make sure we get a JDK.
                 .toLinkedList();
@@ -97,42 +99,23 @@ public class JdkInstallationManager {
     }
 
     /**
-     * Provision a JDK for the specified version.
-     * <p>
-     * If one already exists. That will be returned instead.
-     * <p>
-     * Overload of {@link #provisionJdk(JavaVersion, String, boolean, DownloadListener)} which
-     * only takes a semver version.
-     *
-     * @param semver   An optional semver filter.
-     * @param listener The {@link DownloadListener} to use when provisioning the new JDK. May be <code>null</code>.
-     * @return The JDK home directory.
-     * @throws IOException Thrown if an error occurs whilst provisioning the JDK.
-     */
-    public Path provisionJdk(String semver, boolean jre, @Nullable DownloadListener listener) throws IOException {
-        return provisionJdk(requireNonNull(JavaVersion.parse(semver)), semver, jre, listener);
-    }
-
-    /**
-     * Provision a JDK for the specified version.
+     * Provision a JDK/JRE as per the request.
      * <p>
      * If one already exists. That will be returned instead.
      *
-     * @param version  The java Major version to use.
-     * @param semver   An optional semver filter for the specified major version.
-     * @param listener The {@link DownloadListener} to use when provisioning the new JDK. May be <code>null</code>.
+     * @param request The {@link ProvisionRequest}.
      * @return The JDK home directory.
      * @throws IOException Thrown if an error occurs whilst provisioning the JDK.
      */
-    public Path provisionJdk(JavaVersion version, @Nullable String semver, boolean jre, @Nullable DownloadListener listener) throws IOException {
-        Path existing = findJdk(version, semver, jre);
+    public Path provisionJdk(ProvisionRequest request) throws IOException {
+        Path existing = findJdk(request.version, request.semver, request.preferJre, request.ignoreMacosAArch64);
         if (existing != null) return existing;
 
-        ProvisionResult result = provisioner.provisionJdk(new ProvisionRequest(baseDir, version, semver, jre, ignoreMacosAArch64), listener);
+        ProvisionResult result = provisioner.provisionJdk(baseDir, request);
 
         assert Files.exists(result.baseDir);
 
-        installations.add(new Installation(result.semver, result.isJdk, hashInstallation(result.baseDir), result.baseDir.toAbsolutePath().toString()));
+        installations.add(new Installation(result.semver, result.isJdk, result.architecture, hashInstallation(result.baseDir), result.baseDir.toAbsolutePath().toString()));
         saveManifest();
         return JavaInstall.getHomeDirectory(result.baseDir);
     }
@@ -168,22 +151,17 @@ public class JdkInstallationManager {
     public interface JdkProvisioner {
 
         /**
-         * Provision a JDK with the given Java major version.
+         * Provision a JDK/JRE from the given request.
          *
+         * @param baseDir  The folder to place the JDK/JRE folder
          * @param request  The provision request.
-         * @param listener The {@link DownloadListener} to use when provisioning the new JDK. May be <code>null</code>.
          * @return A {@link ProvisionResult} containing the properties about the provisioned jdk.
          * @throws IOException If there was an error provisioning the JDK.
          */
-        ProvisionResult provisionJdk(ProvisionRequest request, @Nullable DownloadListener listener) throws IOException;
+        ProvisionResult provisionJdk(Path baseDir, ProvisionRequest request) throws IOException;
     }
 
     public static class ProvisionRequest {
-
-        /**
-         * The folder to place the JDK/JRE folder.
-         */
-        public final Path baseFolder;
 
         /**
          * The java Major version to provision.
@@ -199,19 +177,64 @@ public class JdkInstallationManager {
         /**
          * If only a JRE is required.
          */
-        public final boolean jre;
+        public final boolean preferJre;
 
         /**
          * If macOS AArch64 should be treated as x64.
          */
         public final boolean ignoreMacosAArch64;
 
-        public ProvisionRequest(Path baseFolder, JavaVersion version, @Nullable String semver, boolean jre, boolean ignoreMacosAArch64) {
-            this.baseFolder = baseFolder;
-            this.version = version;
-            this.semver = semver;
-            this.jre = jre;
-            this.ignoreMacosAArch64 = ignoreMacosAArch64;
+        public final @Nullable DownloadListener downloadListener;
+
+        public ProvisionRequest(Builder builder) {
+            if (builder.version == null) {
+                throw new IllegalStateException("Expected either a version, or semver filter");
+            }
+
+            version = builder.version;
+            semver = builder.semver;
+            preferJre = builder.preferJre;
+            ignoreMacosAArch64 = builder.ignoreMacosAArch64;
+            downloadListener = builder.downloadListener;
+        }
+
+        public static final class Builder {
+
+            private @Nullable JavaVersion version;
+            private @Nullable String semver;
+            private boolean preferJre;
+            private boolean ignoreMacosAArch64;
+            private @Nullable DownloadListener downloadListener;
+
+            public Builder forVersion(JavaVersion version) {
+                this.version = version;
+                return this;
+            }
+
+            public Builder withSemver(String semver) {
+                forVersion(requireNonNull(JavaVersion.parse(semver)));
+                this.semver = semver;
+                return this;
+            }
+
+            public Builder preferJRE(boolean preferJre) {
+                this.preferJre = preferJre;
+                return this;
+            }
+
+            public Builder ignoreMacosAArch64(boolean ignoreMacosAArch64) {
+                this.ignoreMacosAArch64 = ignoreMacosAArch64;
+                return this;
+            }
+
+            public Builder downloadListener(DownloadListener downloadListener) {
+                this.downloadListener = downloadListener;
+                return this;
+            }
+
+            public ProvisionRequest build() {
+                return new ProvisionRequest(this);
+            }
         }
     }
 
@@ -220,11 +243,13 @@ public class JdkInstallationManager {
         public final String semver;
         public final Path baseDir;
         public final boolean isJdk;
+        public final Architecture architecture;
 
-        public ProvisionResult(String semver, Path baseDir, boolean isJdk) {
+        public ProvisionResult(String semver, Path baseDir, boolean isJdk, Architecture architecture) {
             this.semver = semver;
             this.baseDir = baseDir;
             this.isJdk = isJdk;
+            this.architecture = architecture;
         }
     }
 
@@ -235,6 +260,9 @@ public class JdkInstallationManager {
         public boolean isJdk = false;
 
         @Nullable
+        public Architecture architecture;
+
+        @Nullable
         @JsonAdapter (HashCodeAdapter.class)
         public HashCode hash;
 
@@ -243,10 +271,11 @@ public class JdkInstallationManager {
         public Installation() {
         }
 
-        public Installation(String version, boolean isJdk, @Nullable HashCode hash, String path) {
+        public Installation(String version, boolean isJdk, Architecture architecture, @Nullable HashCode hash, String path) {
             this();
             this.version = version;
             this.isJdk = isJdk;
+            this.architecture = architecture;
             this.hash = hash;
             this.path = path;
         }
